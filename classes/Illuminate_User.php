721dd8f3a51547f9d5f2d6207b9d318e0f2a2be8
Joshy Francis <JoshyFrancis@users.noreply.github.com>
2020-05-16 12:07:39 +0530

    Update Illuminate_User.php

--- classes/Illuminate_User.php
+++ classes/Illuminate_User.php
@@ -8,33 +8,5 @@ Class User extends Model{
         'username', 'email', 'password',
     ];
 	//add updated_at(datetime) column to table users laravel requires it for changing password
-	protected static $user =null;
-	public function __construct(){
-		User::$user=$this;
-    }
-	public static function find($ID){
-		$model=new self;
-		$user=User::$user;
-		$fillable=$user->fillable+$user->names;
-			$sql='SELECT ';
-			$c=0;
-			foreach($fillable as $name){
-				if($c>0){
-					$sql.=',';
-				}
-				$sql.=$name;
-				$c+=1;
-			}
-			$sql.=' FROM '.$user->table.' where '.$user->primaryKey.'=?';
-
-		$rows =\DB::select($sql ,[$ID] );
-		if(count($rows)>0){
-			foreach($fillable as $name){
-				$model->{$name}=$rows[0]->{$name};
-			}
-			$model->{$model->primaryKey}=$ID;
-		}
-		return $model;
-	}
 }
 

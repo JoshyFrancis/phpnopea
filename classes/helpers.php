aff5f4631b2757a7eb49702a982300e83332a3b7
Joshy Francis <JoshyFrancis@users.noreply.github.com>
2020-05-18 09:07:55 +0530

    Update helpers.php

--- classes/helpers.php
+++ classes/helpers.php
@@ -111,6 +111,26 @@ if (!function_exists('getallheaders')){
         return $headers;
     }
 }
+function parse_ini_file_quotes_safe($f){
+    $r=$null;
+    $sec=$null;
+    $f=@file($f);
+    for($i=0;$i<@count($f);$i++){
+        $newsec=0;
+        $w=@trim($f[$i]);
+        if($w){
+         if((!$r) or ($sec)){
+            if((@substr($w,0,1)=="[") and (@substr($w,-1,1))=="]") {$sec=@substr($w,1,@strlen($w)-2);$newsec=1;}
+         }
+         if(!$newsec){
+            $w=@explode("=",$w);$k=@trim($w[0]);unset($w[0]); $v=@trim(@implode("=",$w));
+            if((@substr($v,0,1)=="\"") and (@substr($v,-1,1)=="\"")) {$v=@substr($v,1,@strlen($v)-2);}
+            if($sec) {$r[$sec][$k]=$v;} else {$r[$k]=$v;}
+         }
+        }
+    }
+    return $r;
+}
 function storage_link($target, $link){
 	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 		$mode = is_dir($target) ? 'J' : 'H';

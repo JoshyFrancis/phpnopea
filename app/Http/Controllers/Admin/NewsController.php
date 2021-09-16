403996f0678e53bdcb70f7967a40be25c6fd7372
Joshy Francis <JoshyFrancis@users.noreply.github.com>
2021-09-16 08:32:45 -0700

    Delete app directory

--- app/Http/Controllers/Admin/NewsController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-class NewsController extends Controller{
-    public function index(Request $request){
-        return 'NewsController index';
-    }   
-}
--- app/Http/Controllers/Admin/UserController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers\Admin;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-class UserController extends Controller{
-    public function index(Request $request){
-        return 'UserController index';
-    }   
-}
